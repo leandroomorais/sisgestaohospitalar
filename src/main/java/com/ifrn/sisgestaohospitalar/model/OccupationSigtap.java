5e3d04d0830997ce33a873580ef9fc96868d1e43
leand <leand@DESKTOP-35E91OO>
2020-03-27 15:36:35 -0300

    Criação do Model OccupatonSigtap contendo os atributos de Ocupação da Tabela de Procedimentos do SUS - Sigtap

---
+++ src/main/java/com/ifrn/sisgestaohospitalar/model/OccupationSigtap.java
@@ -0,0 +1,48 @@
+package com.ifrn.sisgestaohospitalar.model;
+/**Classe para objetos do tipo Ocupação da Tabela Sigtap, onde serão contidos atributos e métodos para o mesmo.
+ * @author Leandro Morais
+ * @version 1.0
+ * @since Release 02 da Aplicação
+ */
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class OccupationSigtap {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String codeoccupation;
+	
+	private String numberoccupation;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodeoccupation() {
+		return codeoccupation;
+	}
+
+	public void setCodeoccupation(String codeoccupation) {
+		this.codeoccupation = codeoccupation;
+	}
+
+	public String getNumberoccupation() {
+		return numberoccupation;
+	}
+
+	public void setNumberoccupation(String numberoccupation) {
+		this.numberoccupation = numberoccupation;
+	}
+
+}
