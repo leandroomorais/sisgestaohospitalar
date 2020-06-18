4a1f9939d0f88b54b06282744c8c8f6d9a3dee5d
leandroomorais <leandropinto1@hotmail.com>
2020-06-18 15:18:57 -0300

    Correções de Segurança

---
+++ src/main/java/com/ifrn/sisgestaohospitalar/controller/AtendimentoMedicoController.java
@@ -0,0 +1,15 @@
+package com.ifrn.sisgestaohospitalar.controller;
+
+/**
+ * A classe Controller <code>AtendimentoMedicoController</code> possui os
+ * métodos de controle para acesso da página principal do Módulo de Atendimento
+ * Médico, Adição de Cidadão a Fila de Atendimento e Visualização da Fila de
+ * Atendimento
+ * 
+ * @author Leandro Morais
+ * @version 1.0, 02/11/2019
+ *
+ */
+public class AtendimentoMedicoController {
+
+}
