e11e5e2a55a3918349843d4e64f9fad4ec96393d
leand <leand@DESKTOP-Q9R9TBF>
2021-03-23 15:44:56 -0300

    Ajustes na classe CidadaoService com adição de validação no método save e remoção de métodos de busca

--- src/main/java/com/ifrn/sisgestaohospitalar/service/CidadaoService.java
+++ src/main/java/com/ifrn/sisgestaohospitalar/service/CidadaoService.java
@@ -1,19 +1,14 @@
 package com.ifrn.sisgestaohospitalar.service;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ifrn.sisgestaohospitalar.model.Cidadao;
 import com.ifrn.sisgestaohospitalar.repository.CidadaoRepository;
-
-/**
- * A classe <code>CidadaoService</code> implementa os métodos da Interface
- * CidadaoRepository
- * 
- * @author Leandro Morais
- * @version 1.0, 02/11/2019
- *
- */
+import com.ifrn.sisgestaohospitalar.service.exception.CidadaoJaCadastradoException;
+import com.ifrn.sisgestaohospitalar.service.exception.DataNascimentoMaiorQueDataAtualException;
 
 @Service
 public class CidadaoService {
@@ -27,65 +22,16 @@ public class CidadaoService {
 	 * @param cidadao
 	 */
 	public void save(Cidadao cidadao) {
-		repository.saveAndFlush(cidadao);
+		Optional<Cidadao> optional = repository.findByCpf(cidadao.getCpf());
+		if(optional.isPresent()) {
+			throw new CidadaoJaCadastradoException("Já existe um Cidadão cadastrado com este mesmo CPF");
+		}
+		if(cidadao.getDatanascimento().isAfter(LocalDate.now())) {
+			throw new DataNascimentoMaiorQueDataAtualException("A data de nascimento é maior que a data atual");
+		}
+		repository.save(cidadao);
 	}
 
-	/**
-	 * Deleta os objetos do tipo Cidadao a partir do Id
-	 * 
-	 * @param id
-	 */
-	public void delete(Long id) {
-		repository.deleteById(id);
-	}
 
-	/**
-	 * Retorna a lista de todos os objetos do tipo Cidadao
-	 * 
-	 * @return List<Cidadao>
-	 */
-	public List<Cidadao> findAll() {
-		return repository.findAll();
-	}
-
-	/**
-	 * Retorna o objeto Cidadao a partir do Id
-	 * 
-	 * @param id
-	 * @return Cidadao
-	 */
-	public Cidadao findOne(Long id) {
-		return repository.getOne(id);
-	}
-
-	/**
-	 * Retorna o objeto Cidadao a partir do CNS (Cartão Nacional de Saúde)
-	 * 
-	 * @param cns
-	 * @return Cidadao
-	 */
-	public Cidadao findByCns(String cns) {
-		return repository.findByCns(cns);
-	}
-
-	/**
-	 * Retorna o cidadao a partir do CPF
-	 * 
-	 * @param cpf
-	 * @return Cidadao
-	 */
-	public Cidadao findByCpf(String cpf) {
-		return repository.findByCpf(cpf);
-	}
-
-	/**
-	 * Retorna o Cidadao a partir do nome
-	 * 
-	 * @param nome
-	 * @return Cidadao
-	 */
-	public Cidadao findByNome(String nome) {
-		return repository.findByNome(nome);
-	}
 
 }
