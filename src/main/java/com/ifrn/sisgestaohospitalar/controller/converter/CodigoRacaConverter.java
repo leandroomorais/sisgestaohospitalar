506725f688791fd11cc2423c36dda0601cc09bc6
leand <leand@DESKTOP-Q9R9TBF>
2021-03-23 15:46:41 -0300

    Criação do Conversor CodigoRacaConverter

---
+++ src/main/java/com/ifrn/sisgestaohospitalar/controller/converter/CodigoRacaConverter.java
@@ -0,0 +1,23 @@
+package com.ifrn.sisgestaohospitalar.controller.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import com.ifrn.sisgestaohospitalar.enums.CodigoRaca;
+
+@Component
+public class CodigoRacaConverter implements Converter<String, CodigoRaca> {
+
+	@Override
+	public CodigoRaca convert(String raca) {
+		if (!StringUtils.isEmpty(raca)) {
+			for (CodigoRaca codigoRaca : CodigoRaca.values()) {
+				if (codigoRaca.getCodigo() == Integer.parseInt(raca)) {
+					return codigoRaca;
+				}
+			}
+		}
+		return null;
+	}
+}
