4c06f4dce525530080908f1cbda3ce3c8a154a69
leand <leand@DESKTOP-Q9R9TBF>
2021-03-23 15:41:28 -0300

    Criação do handler exception

---
+++ src/main/java/com/ifrn/sisgestaohospitalar/controller/handler/ControllerAdviceExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.ifrn.sisgestaohospitalar.controller.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+
+@ControllerAdvice
+public class ControllerAdviceExceptionHandler {
+	
+}
