6ea2a9e9561126ca6e6a27af0e49e1435b2e7741
leand <leand@DESKTOP-Q9R9TBF>
2021-03-23 15:40:04 -0300

    Criação da classe Exception CidadaoJaCadastroException que valida se o cidadão com determinado CPF já está cadastrado na base de dados

---
+++ src/main/java/com/ifrn/sisgestaohospitalar/service/exception/CidadaoJaCadastradoException.java
@@ -0,0 +1,11 @@
+package com.ifrn.sisgestaohospitalar.service.exception;
+
+public class CidadaoJaCadastradoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public CidadaoJaCadastradoException(String message) {
+		super(message);	
+	}
+
+}
