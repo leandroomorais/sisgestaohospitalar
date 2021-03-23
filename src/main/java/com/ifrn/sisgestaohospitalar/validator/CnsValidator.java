d0781e3993521d071be40e820331b99f34bec870
leand <leand@DESKTOP-Q9R9TBF>
2021-03-23 15:47:24 -0300

    Criação do Validator CNS que verifica se o número CNS repassado pelo usuário é válido

---
+++ src/main/java/com/ifrn/sisgestaohospitalar/validator/CnsValidator.java
@@ -0,0 +1,26 @@
+package com.ifrn.sisgestaohospitalar.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.ifrn.sisgestaohospitalar.validation.Cns;
+
+public class CnsValidator implements ConstraintValidator<Cns, String> {
+
+	@Override
+	public boolean isValid(String value, ConstraintValidatorContext context) {
+		if (value.matches("[1-2]\\d{10}00[0-1]\\d") || value.matches("[7-9]\\d{14}")) {
+			return somaPonderada(value) % 11 == 0;
+		}
+		return false;
+	}
+	private int somaPonderada(String s) {
+		char[] cs = s.toCharArray();
+		int soma = 0;
+		for (int i = 0; i < cs.length; i++) {
+			soma += Character.digit(cs[i], 10) * (15 - i);
+		}
+		return soma;
+	}
+
+}
