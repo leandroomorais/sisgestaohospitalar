69526a27389b3493a4ad8054f995d4fde5003ca9
leand <leand@DESKTOP-Q9R9TBF>
2021-03-23 15:33:22 -0300

    Ajustes no Model Cidadão

--- src/main/java/com/ifrn/sisgestaohospitalar/model/Cidadao.java
+++ src/main/java/com/ifrn/sisgestaohospitalar/model/Cidadao.java
@@ -1,72 +1,124 @@
 package com.ifrn.sisgestaohospitalar.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ifrn.sisgestaohospitalar.enums.CodigoRaca;
+import com.ifrn.sisgestaohospitalar.validation.Cns;
 
 @Entity
-public class Cidadao {
+public class Cidadao implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Cns(message = "O CNS digitado é inválido")
 	@Column(nullable = false, length = 15)
+	@Size(max = 15, message = "O campo CNS deve conter 15 caracteres")
 	@NotBlank(message = "É necessário preencher o campo CNS")
 	private String cns;
-	
+
+	@Column(nullable = false, length = 11)
+	@Size(max = 11, message = "O campo CPF deve conter 11 caracteres")
+	@NotBlank(message = "É necessário preencher o campo CPF")
+	@CPF(message = "O CPF digitado é inválido")
 	private String cpf;
 
-	@Column(nullable = false)
+	@Column(nullable = false, length = 1)
+	@Size(max = 1)
 	@NotBlank(message = "É necessário preencher o campo SEXO")
 	private String sexo;
 
-	@Column(nullable = false)
+	@Column(nullable = false, length = 50)
+	@Size(max = 50, message = "O campo NOME deve conter no máximo 50 caracteres")
 	@NotBlank(message = "É necessário preencher o campo NOME")
 	private String nome;
 
+	@Column(nullable = false, length = 50)
+	@Size(max = 50, message = "O campo NOME DA MÃE deve conter no máximo 50 caracteres")
+	@NotBlank(message = "É necessário preencher o campo NOME DA MÃE, caso não possua, selecione a opção SEM INFORMAÇÕES")
 	private String nomemae;
 
+	@Column(nullable = false, length = 50)
+	@Size(max = 50, message = "O campo NOME DO PAI deve conter no máximo 50 caracteres")
+	@NotBlank(message = "É necessário preencher o campo NOME DO PAI, caso não possua, selecione a opção SEM INFORMAÇÕES")
 	private String nomepai;
 
-	@NotNull
+	@NotNull(message = "É necessário preencher o campo DATA DE NASCIMENTO")
 	@PastOrPresent(message = "{PastOrPresent.cidadao.dataEntrada}")
 	@DateTimeFormat(iso = ISO.DATE, pattern = "")
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	@Column(name = "datanascimento", nullable = false, columnDefinition = "DATE")
 	private LocalDate datanascimento;
 
+	@NotNull(message = "É necessário selecionar a RAÇA do Cidadão")
 	@Column(length = 2)
-	private int codigoraca;
+	@Enumerated(EnumType.STRING)
+	private CodigoRaca codigoRaca;
 
 	@Column(length = 4)
 	private int codigoetnia;
 
+	@NotNull(message = "É necessário selecionar a NACIONALIDADE do Cidadão")
 	@Column(length = 3)
 	private int codigonacionalidade;
 
 	private String telefone;
 
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 	@Column(length = 40)
+	@Email(message = "Email inválido")
 	private String email;
-	
+
+	@NotNull(message = "É necessário selecionar o MUNICÍPIO DE NASCIMENTO do Cidadão")
 	@OneToOne
 	private Municipio municipioNascimento;
-	
-	@OneToOne(cascade = CascadeType.PERSIST)
+
+	@NotNull(message = "Preencha os campos do ENDEREÇO do Cidadão")
+	@OneToOne(cascade = CascadeType.ALL)
 	private EnderecoCidadao endereco;
+	
+	@PrePersist
+	@PreUpdate
+	private void prePersistUpdate() {
+		
+		cns.replaceAll("\\.|-|/", "");
+		cpf.replaceAll("\\.|-|/", "");
+		cns = cns.toUpperCase();
+		cpf = cpf.toUpperCase();
+		sexo = sexo.toUpperCase();
+		nome = nome.toUpperCase();
+		nomemae = nomemae.toUpperCase();
+		nomepai = nomepai.toUpperCase();
+	}
 
 	public Long getId() {
 		return id;
@@ -81,7 +133,7 @@ public class Cidadao {
 	}
 
 	public void setCns(String cns) {
-		this.cns = cns;
+		this.cns = cns.replaceAll("\\.|-|/", "");
 	}
 
 	public String getCpf() {
@@ -89,7 +141,7 @@ public class Cidadao {
 	}
 
 	public void setCpf(String cpf) {
-		this.cpf = cpf;
+		this.cpf = cpf.replaceAll("\\.|-|/", "");
 	}
 
 	public String getSexo() {
@@ -132,12 +184,12 @@ public class Cidadao {
 		this.datanascimento = datanascimento;
 	}
 
-	public int getCodigoraca() {
-		return codigoraca;
+	public CodigoRaca getCodigoRaca() {
+		return codigoRaca;
 	}
 
-	public void setCodigoraca(int codigoraca) {
-		this.codigoraca = codigoraca;
+	public void setCodigoRaca(CodigoRaca codigoRaca) {
+		this.codigoRaca = codigoRaca;
 	}
 
 	public int getCodigoetnia() {
@@ -188,5 +240,29 @@ public class Cidadao {
 		this.endereco = endereco;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidadao other = (Cidadao) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
