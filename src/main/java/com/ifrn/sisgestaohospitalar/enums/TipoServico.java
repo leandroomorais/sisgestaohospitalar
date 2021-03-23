40f268277b91fd58e5301f97a97034f1d76fa06e
leand <leand@DESKTOP-Q9R9TBF>
2021-03-23 15:38:06 -0300

    Ajustes no ENUM TipoServico

--- src/main/java/com/ifrn/sisgestaohospitalar/enums/TipoServico.java
+++ src/main/java/com/ifrn/sisgestaohospitalar/enums/TipoServico.java
@@ -1,5 +1,18 @@
 package com.ifrn.sisgestaohospitalar.enums;
 
 public enum TipoServico {
-	EscutaInicial, AtendimentoMedico, AdminMedicamentos, Observacao
+	EscutaInicial("Esculta Inicial"), AtendimentoMedico("Atendimento Médico"),
+	AdminMedicamentos("Administração de Medicamentos"), Observacao("Observação");
+
+	private String descricao;
+	
+	
+	TipoServico(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}	
 }
