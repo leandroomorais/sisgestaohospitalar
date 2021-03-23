839c2a5554cff828da6ad7c72524b592ad50b0a5
robson-ribeiro-da-silva <robson_r.silva@hotmail.com.br>
2021-03-23 08:25:40 -0300

    Commit - Teste

--- src/main/java/com/ifrn/sisgestaohospitalar/SisgestaohospitalarApplication.java
+++ src/main/java/com/ifrn/sisgestaohospitalar/SisgestaohospitalarApplication.java
@@ -8,6 +8,8 @@ public class SisgestaohospitalarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SisgestaohospitalarApplication.class, args);
+		
+		System.out.println("Iniciando Projeto");
 	}
 
 }
