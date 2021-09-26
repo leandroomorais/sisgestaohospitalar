71962038ac41691b350fdfd68297e27fd1031a17
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2021-09-26 15:54:37 -0300

    Criação do método salvarRegistro

--- src/main/java/com/ifrn/sisgestaohospitalar/controller/PrescricaoController.java
+++ src/main/java/com/ifrn/sisgestaohospitalar/controller/PrescricaoController.java
@@ -1,5 +1,6 @@
 package com.ifrn.sisgestaohospitalar.controller;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.ifrn.sisgestaohospitalar.dto.PrescricaoDTO;
 import com.ifrn.sisgestaohospitalar.model.Atendimento;
 import com.ifrn.sisgestaohospitalar.model.Prescricao;
+import com.ifrn.sisgestaohospitalar.model.RegistroAdministracao;
 import com.ifrn.sisgestaohospitalar.repository.AtendimentoRepository;
 import com.ifrn.sisgestaohospitalar.repository.PrescricaoRepository;
 
@@ -51,6 +53,28 @@ public class PrescricaoController {
 		prescricaoRepository.delete(prescricao);
 		return ResponseEntity.ok().build();
 
+	}
+	
+	@PostMapping("/registro")
+	public ResponseEntity<?> salvarRegistro(@Valid RegistroAdministracao registroAdministracao, BindingResult result) {
+		System.out.println(registroAdministracao.toString());
+		Map<String, String> errors = new HashMap<>();
+		if (result.hasErrors()) {
+			for (FieldError error : result.getFieldErrors()) {
+				errors.put(error.getField(), error.getDefaultMessage());
+			}
+			return ResponseEntity.unprocessableEntity().body(errors);
+		}
+
+		Optional<Prescricao> optional = prescricaoRepository.findById(registroAdministracao.getIdPrescricao());
+		if (optional.isPresent()) {
+			Prescricao prescricao = optional.get();
+			registroAdministracao.setDataAdministracao(LocalDateTime.now());
+			prescricao.getRegistrosAdministracao().add(registroAdministracao);
+			prescricaoRepository.saveAndFlush(prescricao);
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.badRequest().build();
 	}
 
 	@PostMapping("/editar")
