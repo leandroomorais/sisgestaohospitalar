d672c516f380f32e955935503dd6020792934f57
AntonioAlmeidaRego <antonio.alm_pdf@hotmail.com>
2021-11-20 16:58:11 -0300

    add ajustes

--- src/main/resources/static/assets/js/docs/docs.js
+++ src/main/resources/static/assets/js/docs/docs.js
@@ -1,24 +1,28 @@
 class Docs{
     static createHeader(templateDoc){
-        templateDoc.createStyle({ value: loadCSS() })
-        templateDoc.createHeader({
-            value: `MINISTÉRIO DA SAÚDE
-        ESTADO DE RIO GRANDE DO NORTE
-        MUNICÍPIO DE SEVERIANO MELO
-        UNIDADE DE SAÚDE Hospital Maternidade Municipal de Severiano Melo`})
+        //templateDoc.createStyle({ value: loadCSS() })
+        templateDoc.createHTML({
+            innerHTML: `<div class="header">
+            <p>MINISTÉRIO DA SAÚDE<br> ESTADO DE RIO GRANDE DO NORTE<br> MUNICÍPIO DE SEVERIANO MELO<br> UNIDADE DE
+                SAÚDE Hospital Maternidade Municipal de Severiano Melo</p>
+        </div>`})
     }
     
-    static docReceituario(){
+    static docReceituario(innerHTML){
         const doc = new Doc({ url: '', target: '', features: 'height=2970,width=2100' })
         const document = doc.getDoc()
         const templateDoc = new TemplateDoc(document)
-        templateDoc.openHtml({ label: "Documento gerado pelo SGH" })
+        templateDoc.openHtml({ title: "Documento gerado pelo SGH", links: [
+            {rel: 'stylesheet', type: 'text/css', href: 'http://localhost:9090/assets/css/bootstrap.min.css'},
+            {rel: 'stylesheet', type: 'text/css', href: 'http://localhost:9090/assets/css/atlantis.css'},
+        ]})
         templateDoc.openBody()
         this.createHeader(templateDoc)
-        templateDoc.createTitle({value: `Receituário`, attrsChild: { style: "font-weight: bold" }, attrsFather: { class: 'paragraph-left' }, attrsFatherParent: { class: 'space-simple-bottom' } })
-        templateDoc.createText({value: `Nome do Paciente: Antonio Almeida`, attrsChild: { style: "font-weight: bold" }, attrsFather: { class: 'paragraph-left' }, attrsFatherParent: { class: 'space-simple-bottom' } })
-        templateDoc.createText({value: `CPF.: 000.000.000-00`, attrsChild: { style: "font-weight: bold" }, attrsFather: { class: 'paragraph-left' }, attrsFatherParent: { class: 'space-simple-bottom' } })
-        templateDoc.createText({value: `Medicamentos`, attrsChild: { style: "font-weight: bold" }, attrsFather: { class: 'paragraph-left' }, attrsFatherParent: { class: 'space-simple-bottom' } })
+        templateDoc.createHTML({innerHTML: `<div class="space-simple-bottom"><div class="title"><p style="font-weight: bold">Receituário</p></div></div>`})
+        templateDoc.createHTML({innerHTML: `<div class="space-simple-bottom"><div class="paragraph-left"><p style="font-weight: bold">Nome do Paciente: Antonio Almeida</p></div></div>`})
+        templateDoc.createHTML({innerHTML: `<div class="space-simple-bottom"><div class="paragraph-left"><p style="font-weight: bold">CPF.: 000.000.000-00</p></div></div>`})
+        templateDoc.createHTML({innerHTML: `<div class="space-simple-bottom"><div class="paragraph-left"><p style="font-weight: bold">Medicamentos</p></div></div>`})
+        templateDoc.createHTML({innerHTML: innerHTML})
         this.close(templateDoc, doc)
     }
 
