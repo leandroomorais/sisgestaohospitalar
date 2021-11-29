30bdf4daea1bc91b16da4dceae3a6f7662f6b964
AntonioAlmeidaRego <antonio.alm_pdf@hotmail.com>
2021-11-29 20:02:10 -0300

    finalizado o imprimir detalhe atendimento

---
+++ src/main/resources/static/assets/js/atendimento/imprimir-atendimento.js
@@ -0,0 +1,23 @@
+function imprimirAtendimento(){
+    const div = document.getElementsByClassName('page-inner').item(1).children.item(0).children.item(0).children.item(2)
+    console.log(div)
+    const doc = new Doc({ url: '', target: '', features: 'height=2970,width=2100' })
+    const templateDoc = new TemplateDoc(doc)
+    
+    templateDoc.getHtml({ title: "Documento gerado pelo SGH", links: [
+        {type: 'text/css', href: 'http://localhost:9090/assets/css/bootstrap.min.css'},
+        {type: 'text/css', href: 'http://localhost:9090/assets/css/atlantis.css'},
+    ]})
+    templateDoc.createStyle({value: `@media print {
+        #print {
+            display: none;
+        }
+    }`})
+    //templateDoc.getBody({class: 'card'})
+    templateDoc.createHTML({innerHTML: div.innerHTML})
+    templateDoc.createHTML({innerHTML: `<div class="card" id="print">
+        <div  class="card-footer">
+            <button class="btn btn-primary" onclick="window.print()">Imprimir</button>
+        </div>
+    </div>`})
+}
