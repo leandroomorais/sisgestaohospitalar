4a6a50b2df4128d528d76526c8daae0eca7acd03
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2021-12-17 22:40:43 -0300

    Alterações

--- src/main/java/com/ifrn/sisgestaohospitalar/repository/ProfissionalRepository.java
+++ src/main/java/com/ifrn/sisgestaohospitalar/repository/ProfissionalRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.ifrn.sisgestaohospitalar.enums.TipoProfissional;
 import com.ifrn.sisgestaohospitalar.model.Profissional;
 
@@ -21,8 +20,10 @@ public interface ProfissionalRepository extends JpaRepository<Profissional, Long
 
 	Profissional findByEmail(String email);
 
-	@Query(value = "select * from profissional p where p.nome "
-				+" like concat('%', :name, '%')", nativeQuery = true)
+	@Query(value = "select * from profissional p where p.nome " + " like concat('%', :name, '%')", nativeQuery = true)
 	Profissional findByNome(@Param("name") String nome);
+
+	@Query(nativeQuery = true, value = "select * from profissional p inner join profissional_lotacao pl on p.id = pl.profissional_id inner join lotacao lt on pl.lotacao_id = lt.id where (lt.codigocbo = '223505' or lt.codigocbo = '225125' or lt.codigocbo = '322205' or lt.codigocbo = '322230')")
+	List<Profissional> searchSelectOptions();
 
 }
