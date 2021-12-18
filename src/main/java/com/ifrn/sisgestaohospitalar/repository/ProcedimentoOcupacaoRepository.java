85cae755d0eedb5fcd436362a3f16f6828521d22
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2021-12-18 00:08:34 -0300

    Criação de método que verifica se procedimento é compatível com o CBO do profissional

--- src/main/java/com/ifrn/sisgestaohospitalar/repository/ProcedimentoOcupacaoRepository.java
+++ src/main/java/com/ifrn/sisgestaohospitalar/repository/ProcedimentoOcupacaoRepository.java
@@ -6,14 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.ifrn.sisgestaohospitalar.model.ProcedimentoOcupacao;
 
 @Repository
 public interface ProcedimentoOcupacaoRepository extends JpaRepository<ProcedimentoOcupacao, Long> {
 	List<ProcedimentoOcupacao> findByCodigoProcedimento(Long codigoProcedimento);
 
-	@Query(nativeQuery = true, value = "INSERT INTO procedimento_ocupacao()")
-	public void testsalve(@Param("id") Long id, @Param("codigoProcedimento") Long codigoProcedimento,
-			@Param("codigoOcupacap") String codigoOcupacao, @Param("competencia") String competencia);
+	@Query(nativeQuery = true, value = "select * from procedimento p inner join procedimento_ocupacao po on p.codigo = po.codigo_procedimento where p.codigo = :codigoProcedimento and po.codigo_ocupacao = :codigoOcupacao group by p.codigo")
+	ProcedimentoOcupacao verificaCBO(@Param("codigoProcedimento") Long codigoProcedimento,
+			@Param("codigoOcupacao") String codigoOcupacao);
 }
