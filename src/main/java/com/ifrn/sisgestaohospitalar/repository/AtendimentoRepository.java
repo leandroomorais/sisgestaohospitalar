09d33fc127aa551fbe55e9e4e785f2eb2b734f57
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2022-03-04 22:56:23 -0300

    Outras

--- src/main/java/com/ifrn/sisgestaohospitalar/repository/AtendimentoRepository.java
+++ src/main/java/com/ifrn/sisgestaohospitalar/repository/AtendimentoRepository.java
@@ -29,7 +29,7 @@ public interface AtendimentoRepository extends JpaRepository<Atendimento, Long> 
 	@Query("select a from Atendimento a where a.cidadao.nome like %:search% or a.profissionalDestino like %:search%")
 	Page<Atendimento> findByCidadaoOrProfissionalOrTipoServico(@Param("search") String search, Pageable pageable);
 
-	@Query("select a from Atendimento a where SUBSTRING(a.dataEntrada,1,7) = :mes")
-	List<Atendimento> findByMes(@Param("mes") String mes);
+	@Query("select a from Atendimento a where year(a.dataEntrada) = 2022 and month(a.dataEntrada) = 3")
+	List<Atendimento> findByMes( @Param("ano") String ano, @Param("mes") String mes);
 
 }
