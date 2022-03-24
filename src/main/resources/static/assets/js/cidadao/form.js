4087acc306f460d4893ecdd1e9b8ddf61ffcaf04
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2022-03-24 09:54:13 -0300

    Validação da busca do Cidadão - Correção de bugs

--- src/main/resources/static/assets/js/cidadao/form.js
+++ src/main/resources/static/assets/js/cidadao/form.js
@@ -72,189 +72,200 @@ $("#form-busca").submit(function(evt) {
 	info.nome = $("#nome-busca").val();
 	info.dataNascimento = $("#dataNascimentoBusca").val();
 	limparFormularioCidadao();
-	$.ajax({
-		method: "POST",
-		url: "/cidadao/busca-local",
-		data: info,
-		beforeSend: function() {
-			removeMsgErro();
-		},
-		success: function(data) {
-			idCidadao = data.id;
-			$("#form-cidadao-div").fadeOut(500);
-			limpaFormularioBusca();
-			$("#pesquisa-status").text("Cidadão encontrado na base Local");
-			limpaFormularioBusca();
-			$("#info-cidadao").fadeIn(500);
-			$("#card-info-cidadao").empty().append(creatCardDetalheCidadao(data));
-		},
-		statusCode: {
-			//Code Here
-			404: function() {
-				$.ajax({
-					method: "POST",
-					url: "/cidadao/busca-cadsus",
-					data: info,
-					beforSend: function() {
-						removeMsgErro();
-					},
-					success: function(data) {
-						limpaFormularioBusca();
-						$("#pesquisa-status").text("Cidadão encontrado na base do CADSUS");
-						$("#form-cidadao-div").fadeIn(500);
-						if (data.nome != null) {
-							$("#nome").val(data.nome);
-						} else {
-							preenchimentoObrigatorio($("#small-nome"), $('label[for="' + $("#nome").attr("id") + '"]').text());
-						}
-						if (data.dataNascimento != null) {
-							$("#datanascimento").val(data.dataNascimento);
-						} else {
-							preenchimentoObrigatorio($("#small-dataNascimento"), $('label[for="' + $("#datanascimento").attr("id") + '"]').text());
-						}
-						if (data.nomeMae != null) {
-							$("#nomemae").val(data.nomeMae);
-							if (data.nomeMae == "SEM INFORMAÇÃO") {
-								$("#semInfo-mae").attr("checked", "checked");
-							}
-						} else {
-							preenchimentoObrigatorio($("#small-nomeMae"), $('label[for="' + $("#nomemae").attr("id") + '"]').text());
-						}
-						if (data.nomePai != null) {
-							$("#nomepai").val(data.nomePai);
-							if (data.nomePai == "SEM INFORMAÇÃO") {
-								$("#semInfo-pai").attr("checked", "checked");
-							}
-						} else {
-							preenchimentoObrigatorio($("#small-nomePai"), $('label[for="' + $("#nomepai").attr("id") + '"]').text());
-						}
-						if (data.sexo != null) {
-							$("#sexo").val(data.sexo).change();
-						} else {
-							preenchimentoObrigatorio($("#small-sexo"), $('label[for="' + $("#sexo").attr("id") + '"]').text());
-						}
-						if (data.codigoRaca != null) {
-							if (data.codigoRaca == "BRANCA") {
-								$("#codigoraca").val(1).change();
-							}
-							if (data.codigoRaca == "PRETA") {
-								$("#codigoraca").val(2).change();
-							}
-							if (data.codigoRaca == "PARDA") {
-								$("#codigoraca").val(3).change();
-							}
-							if (data.codigoRaca == "AMARELA") {
-								$("#codigoraca").val(4).change();
-							}
-							if (data.codigoRaca == "INDIGENA") {
-								$("#codigoraca").val(5).change();
-							}
-							if (data.codigoRaca == "SEMINFORMACAO") {
-								$("#codigoraca").val(99).change();
-							}
-						} else {
-							preenchimentoObrigatorio($("#small-codigoraca"), $('label[for="' + $("#codigoraca").attr("id") + '"]').text());
-						}
-
-						if (data.municipioNascimento != null) {
-							$("#municipioNascimento").val(data.municipioNascimento.nomeMunicipioSiglaUF);
-							$("#id-municipioNascimento").val(data.municipioNascimento.id);
-						} else {
-							preenchimentoObrigatorio($("#small-municipioNascimento"), $('label[for="' + $("#municipioNascimento").attr("id") + '"]').text());
-						}
-
-						if (data.codigoNacionalidade != null) {
-							$("#codigonacionalidade").val(data.codigoNacionalidade).change();
-						} else {
-							$("#codigonacionalidade").val(10).change();
-						}
-
-						if (data.cns != null) {
-							$("#cns").val(data.cns);
-						} else {
-							preenchimentoObrigatorio($("#small-cns"), $('label[for="' + $("#cns").attr("id") + '"]').text());
-						}
-
-						if (data.cpf != null) {
-							$("#cpf").val(data.cpf);
-						} else {
-							preenchimentoObrigatorio($("#small-cpf"), $('label[for="' + $("#cpf").attr("id") + '"]').text());
-						}
-
-						if (data.endereco.cep != null) {
-							$("#cep").val(data.endereco.cep);
-							$("#nameCep").val(data.endereco.cep);
-						} else {
-							preenchimentoObrigatorio($("#small-endereco-cep"), $('label[for="' + $("#cep").attr("id") + '"]').text());
-						}
-
-						if (data.endereco.municipio.estado.nome != null) {
-							$("#nomeEstado").val(data.endereco.municipio.estado.nome);
-							$("#estadoId").val(data.endereco.municipio.estado.id);
-						} else {
-							preenchimentoObrigatorio($("#small-estado"), $('label[for="' + $("#nomeEstado").attr("id") + '"]').text());
-						}
-
-						if (data.endereco.municipio.nomeMunicipio != null) {
-							$("#nomemunicipio").val(data.endereco.municipio.nomeMunicipio);
-							$("#endereco-municipio").val(data.endereco.municipio.id);
-						} else {
-							preenchimentoObrigatorio($("#small-nomemunicipio"), $('label[for="' + $("#nomemunicipio").attr("id") + '"]').text());
-						}
-
-						if (data.endereco.logradouro.descricao != null) {
-							$("#desc-logradouro").val(data.endereco.logradouro.descricao);
-							$("#endereco-logradouro").val(data.endereco.logradouro.codigo);
-						} else {
-							preenchimentoObrigatorio($("#small-endereco-logradouro"), $('label[for="' + $("#desc-logradouro").attr("id") + '"]').text())
-						}
-
-						if (data.endereco.nomeLogradouro != null) {
-							$("#endereco-nomeLogradouro").val(data.endereco.nomeLogradouro);
-						} else {
-							preenchimentoObrigatorio($("#small-endereco-nomeLogradouro"));
-						}
-
-						if (data.endereco.numero != null) {
-							$("#endereco-numero").val(data.endereco.numero);
-							if (data.endereco.numero == "S/N") {
-								$("#semNumero").attr("checked", "checked");
-							}
-						} else {
-							preenchimentoObrigatorio($("#small-numero"));
-						}
-
-						if (data.endereco.bairro != null) {
-							$("#endereco-bairro").val(data.endereco.bairro);
-						} else {
-							preenchimentoObrigatorio($("#small-bairro"), $('label[for="' + $("#endereco-bairro").attr("id") + '"]').text());
-						}
-
-						if (data.endereco.complemento != null) {
-							$("#endereco-complemento").val(data.endereco.complemento);
-						}
-					},
-					statusCode: {
-						404: function() {
-							$("#form-cidadao-div").fadeIn(500);
-							$("#pesquisa-status").text("Não encontrado");
-							swal("Não encontrado!", "Não localizamos o Cidadão em nossas bases. Por favor preencha os dados do formulário.", {
-								icon: "error",
-								buttons: {
-									confirm: {
-										className: 'btn btn-danger',
-
-									}
-								},
-							}).then(function() {
-								$("#card-pesquisa").fadeOut(500);
-							})
+	if (info.cns == "" && info.cpf == "" && info.nome == "" && info.dataNascimento == "") {
+		notificacao('Atenção!', 'Preencha um dos campos da pesquisa', 'top', 'right', 'warning', 'withicon', '#', '');
+	} else if (info.cns != "" && info.cns.length < 15) {
+		notificacao('Atenção!', 'Preencha o campo CNS corretamente', 'top', 'right', 'warning', 'withicon', '#', '');
+	} else if (info.cpf != "" && info.cpf.length < 11) {
+		notificacao('Atenção!', 'Preencha o campo CPF corretamente', 'top', 'right', 'warning', 'withicon', '#', '');
+	} else if (info.nome == "" && info.dataNascimento.length == 10 || info.nome.length > 1 && info.dataNascimento == "") {
+		notificacao('Atenção!', 'Preencha os campos Nome e Data de nascimento corretamente', 'top', 'right', 'warning', 'withicon', '#', '');
+	} else {
+		$.ajax({
+			method: "POST",
+			url: "/cidadao/busca-local",
+			data: info,
+			beforeSend: function() {
+				console.log(info);
+				removeMsgErro();
+			},
+			success: function(data) {
+				idCidadao = data.id;
+				$("#form-cidadao-div").fadeOut(500);
+				limpaFormularioBusca();
+				$("#pesquisa-status").text("Cidadão encontrado na base Local");
+				limpaFormularioBusca();
+				$("#info-cidadao").fadeIn(500);
+				$("#card-info-cidadao").empty().append(creatCardDetalheCidadao(data));
+			},
+			statusCode: {
+				//Code Here
+				404: function() {
+					$.ajax({
+						method: "POST",
+						url: "/cidadao/busca-cadsus",
+						data: info,
+						beforSend: function() {
+							removeMsgErro();
 						},
-					}
-				})
-			}
-		},
-	})
+						success: function(data) {
+							limpaFormularioBusca();
+							$("#pesquisa-status").text("Cidadão encontrado na base do CADSUS");
+							$("#form-cidadao-div").fadeIn(500);
+							if (data.nome != null) {
+								$("#nome").val(data.nome);
+							} else {
+								preenchimentoObrigatorio($("#small-nome"), $('label[for="' + $("#nome").attr("id") + '"]').text());
+							}
+							if (data.dataNascimento != null) {
+								$("#datanascimento").val(data.dataNascimento);
+							} else {
+								preenchimentoObrigatorio($("#small-dataNascimento"), $('label[for="' + $("#datanascimento").attr("id") + '"]').text());
+							}
+							if (data.nomeMae != null) {
+								$("#nomemae").val(data.nomeMae);
+								if (data.nomeMae == "SEM INFORMAÇÃO") {
+									$("#semInfo-mae").attr("checked", "checked");
+								}
+							} else {
+								preenchimentoObrigatorio($("#small-nomeMae"), $('label[for="' + $("#nomemae").attr("id") + '"]').text());
+							}
+							if (data.nomePai != null) {
+								$("#nomepai").val(data.nomePai);
+								if (data.nomePai == "SEM INFORMAÇÃO") {
+									$("#semInfo-pai").attr("checked", "checked");
+								}
+							} else {
+								preenchimentoObrigatorio($("#small-nomePai"), $('label[for="' + $("#nomepai").attr("id") + '"]').text());
+							}
+							if (data.sexo != null) {
+								$("#sexo").val(data.sexo).change();
+							} else {
+								preenchimentoObrigatorio($("#small-sexo"), $('label[for="' + $("#sexo").attr("id") + '"]').text());
+							}
+							if (data.codigoRaca != null) {
+								if (data.codigoRaca == "BRANCA") {
+									$("#codigoraca").val(1).change();
+								}
+								if (data.codigoRaca == "PRETA") {
+									$("#codigoraca").val(2).change();
+								}
+								if (data.codigoRaca == "PARDA") {
+									$("#codigoraca").val(3).change();
+								}
+								if (data.codigoRaca == "AMARELA") {
+									$("#codigoraca").val(4).change();
+								}
+								if (data.codigoRaca == "INDIGENA") {
+									$("#codigoraca").val(5).change();
+								}
+								if (data.codigoRaca == "SEMINFORMACAO") {
+									$("#codigoraca").val(99).change();
+								}
+							} else {
+								preenchimentoObrigatorio($("#small-codigoraca"), $('label[for="' + $("#codigoraca").attr("id") + '"]').text());
+							}
+
+							if (data.municipioNascimento != null) {
+								$("#municipioNascimento").val(data.municipioNascimento.nomeMunicipioSiglaUF);
+								$("#id-municipioNascimento").val(data.municipioNascimento.id);
+							} else {
+								preenchimentoObrigatorio($("#small-municipioNascimento"), $('label[for="' + $("#municipioNascimento").attr("id") + '"]').text());
+							}
+
+							if (data.codigoNacionalidade != null) {
+								$("#codigonacionalidade").val(data.codigoNacionalidade).change();
+							} else {
+								$("#codigonacionalidade").val(10).change();
+							}
+
+							if (data.cns != null) {
+								$("#cns").val(data.cns);
+							} else {
+								preenchimentoObrigatorio($("#small-cns"), $('label[for="' + $("#cns").attr("id") + '"]').text());
+							}
+
+							if (data.cpf != null) {
+								$("#cpf").val(data.cpf);
+							} else {
+								preenchimentoObrigatorio($("#small-cpf"), $('label[for="' + $("#cpf").attr("id") + '"]').text());
+							}
+
+							if (data.endereco.cep != null) {
+								$("#cep").val(data.endereco.cep);
+								$("#nameCep").val(data.endereco.cep);
+							} else {
+								preenchimentoObrigatorio($("#small-endereco-cep"), $('label[for="' + $("#cep").attr("id") + '"]').text());
+							}
+
+							if (data.endereco.municipio.estado.nome != null) {
+								$("#nomeEstado").val(data.endereco.municipio.estado.nome);
+								$("#estadoId").val(data.endereco.municipio.estado.id);
+							} else {
+								preenchimentoObrigatorio($("#small-estado"), $('label[for="' + $("#nomeEstado").attr("id") + '"]').text());
+							}
+
+							if (data.endereco.municipio.nomeMunicipio != null) {
+								$("#nomemunicipio").val(data.endereco.municipio.nomeMunicipio);
+								$("#endereco-municipio").val(data.endereco.municipio.id);
+							} else {
+								preenchimentoObrigatorio($("#small-nomemunicipio"), $('label[for="' + $("#nomemunicipio").attr("id") + '"]').text());
+							}
+
+							if (data.endereco.logradouro.descricao != null) {
+								$("#desc-logradouro").val(data.endereco.logradouro.descricao);
+								$("#endereco-logradouro").val(data.endereco.logradouro.codigo);
+							} else {
+								preenchimentoObrigatorio($("#small-endereco-logradouro"), $('label[for="' + $("#desc-logradouro").attr("id") + '"]').text())
+							}
+
+							if (data.endereco.nomeLogradouro != null) {
+								$("#endereco-nomeLogradouro").val(data.endereco.nomeLogradouro);
+							} else {
+								preenchimentoObrigatorio($("#small-endereco-nomeLogradouro"));
+							}
+
+							if (data.endereco.numero != null) {
+								$("#endereco-numero").val(data.endereco.numero);
+								if (data.endereco.numero == "S/N") {
+									$("#semNumero").attr("checked", "checked");
+								}
+							} else {
+								preenchimentoObrigatorio($("#small-numero"));
+							}
+
+							if (data.endereco.bairro != null) {
+								$("#endereco-bairro").val(data.endereco.bairro);
+							} else {
+								preenchimentoObrigatorio($("#small-bairro"), $('label[for="' + $("#endereco-bairro").attr("id") + '"]').text());
+							}
+
+							if (data.endereco.complemento != null) {
+								$("#endereco-complemento").val(data.endereco.complemento);
+							}
+						},
+						statusCode: {
+							404: function() {
+								$("#form-cidadao-div").fadeIn(500);
+								$("#pesquisa-status").text("Não encontrado");
+								swal("Não encontrado!", "Não localizamos o Cidadão em nossas bases. Por favor preencha os dados do formulário.", {
+									icon: "error",
+									buttons: {
+										confirm: {
+											className: 'btn btn-danger',
+
+										}
+									},
+								}).then(function() {
+									$("#card-pesquisa").fadeOut(500);
+								})
+							},
+						}
+					})
+				}
+			},
+		})
+	}
 });
 
 //Muda o formulário ao clicar no botão de pesquisa por CPF
