9cad5488042dcddcbe9df9d6e324d4d84eafcfa5
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2022-03-28 01:55:09 -0300

    Ajustes antropometria

--- src/main/resources/static/assets/js/antropometria/antropometria.js
+++ src/main/resources/static/assets/js/antropometria/antropometria.js
@@ -19,83 +19,16 @@ $("#form-antropometria").submit(function(evt) {
 			removeInvalidFedbackAntropometria();
 		},
 		success: function() {
+			limpaAntropometria();
 			$("#table-antropometrias").DataTable().ajax.reload();
-			$.notify({
-				// options
-				icon: 'flaticon-success',
-				title: 'SUCESSO',
-				message: 'O registro de Antropometria foi salvo',
-				target: '_blank'
-			}, {
-				// settings
-				element: 'body',
-				position: null,
-				type: "success",
-				allow_dismiss: true,
-				newest_on_top: false,
-				showProgressbar: false,
-				placement: {
-					from: "top",
-					align: "right"
-				},
-				offset: 20,
-				spacing: 10,
-				z_index: 1031,
-				delay: 5000,
-				timer: 1000,
-				url_target: '_blank',
-				mouse_over: null,
-				animate: {
-					enter: 'animated fadeInDown',
-					exit: 'animated fadeOutUp'
-				},
-				onShow: null,
-				onShown: null,
-				onClose: null,
-				onClosed: null,
-				icon_type: 'class',
-			});
+			notificacao('Sucesso!', 'O registro de Antropometria foi salvo', 'top', 'right', 'success', 'withicon', '#', '');
 			cardInfoCidadao(idAtendimento);
 		},
 		statusCode: {
 			422: function(xhr) {
 				var errors = $.parseJSON(xhr.responseText);
 				$.each(errors, function(key, val) {
-					$.notify({
-						// options
-						icon: 'flaticon-exclamation',
-						title: 'ATENÇÃO',
-						message: val,
-						target: '_blank'
-					}, {
-						// settings
-						element: 'body',
-						position: null,
-						type: "danger",
-						allow_dismiss: true,
-						newest_on_top: false,
-						showProgressbar: false,
-						placement: {
-							from: "top",
-							align: "right"
-						},
-						offset: 20,
-						spacing: 10,
-						z_index: 1031,
-						delay: 5000,
-						timer: 1000,
-						url_target: '_blank',
-						mouse_over: null,
-						animate: {
-							enter: 'animated fadeInDown',
-							exit: 'animated fadeOutUp'
-						},
-						onShow: null,
-						onShown: null,
-						onClose: null,
-						onClosed: null,
-						icon_type: 'class',
-					});
+					notificacao('Atenção!', val, 'top', 'right', 'danger', 'withicon', '#', '');
 					$("input[name='" + key + "']").parent().parent().addClass("has-error has-feedback");
 				})
 			}
