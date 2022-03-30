975914c506c526af2687e2284892e31b36dcceb4
leand <leand@DESKTOP-PD9U99U>
2022-03-30 01:05:58 -0300

    Ajuste form-atendimento

--- src/main/resources/static/assets/js/atendimento/form-atendimento.js
+++ src/main/resources/static/assets/js/atendimento/form-atendimento.js
@@ -5,9 +5,7 @@ var inicioConsulta;
 $(document).ready(function() {
 
 	idAtendimento = $("#id-atendimento").val();
-
 	verificaConsulta();
-
 	ocultarAlergia();
 	ocultarDoenca();
 
@@ -49,40 +47,27 @@ $(document).ready(function() {
 		content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
 	});
 	//Fim da função que inicia o TinyMCE
-
 	$("#adicionar-a-lista").hide();
 	$("#form-observacao").hide();
-
 	atualizaPrescricoes();
 	atualizaAtestados();
-
 	//Chamada da função 
 	atualizaAlergia();
-
 	//Chamada da função 
 	atualizaDoenca();
-
 	// Funções dos Exames
 	atualizaProcedimentoExame();
 	atualizaExames();
 	//tabelaTodosExame();
 	//ExamesSolicitados();
-
 	atualizaTodosExames();
-
 	//Chamada da Função
 	atualizaProcedimento();
-
-
 	//Chamada da função 
 	atulizaMedicamentoUsoContinuo();
-
 	atualizaAntropometria();
-
 	//Chamada da Função
 	atulizaMedicamentoEmUso();
-
-
 	cardInfoCidadao(idAtendimento);
 
 
@@ -116,353 +101,25 @@ $("#form-consulta").submit(function(evt) {
 
 		},
 		success: function() {
-			$.notify({
-				// options
-				icon: 'flaticon-success',
-				title: 'SUCESSO',
-				message: 'A Consulta foi salva',
-				target: '_blank'
-			}, {
-				// settings
-				element: 'body',
-				position: null,
-				type: "success",
-				allow_dismiss: true,
-				newest_on_top: false,
-				showProgressbar: false,
-				placement: {
-					from: "top",
-					align: "right"
-				},
-				offset: 20,
-				spacing: 10,
-				z_index: 1031,
-				delay: 5000,
-				timer: 1000,
-				url_target: '_blank',
-				mouse_over: null,
-				animate: {
-					enter: 'animated fadeInDown',
-					exit: 'animated fadeOutUp'
-				},
-				onShow: null,
-				onShown: null,
-				onClose: null,
-				onClosed: null,
-				icon_type: 'class',
-			});
-
+			notificacao('Sucesso!', 'Consulta salva', 'top', 'right', 'success', 'withicon', '#', '');
+			cardInfoCidadao(idAtendimento);
 			verificaConsulta();
-
 		},
 
 		statusCode: {
 			422: function(xhr) {
 				var errors = $.parseJSON(xhr.responseText);
 				$.each(errors, function(key, val) {
-					$.notify({
-						// options
-						icon: 'flaticon-exclamation',
-						title: 'ATENÇÃO',
-						message: val,
-						target: '_blank'
-					}, {
-						// settings
-						element: 'body',
-						position: null,
-						type: "danger",
-						allow_dismiss: true,
-						newest_on_top: false,
-						showProgressbar: false,
-						placement: {
-							from: "top",
-							align: "right"
-						},
-						offset: 20,
-						spacing: 10,
-						z_index: 1031,
-						delay: 5000,
-						timer: 1000,
-						url_target: '_blank',
-						mouse_over: null,
-						animate: {
-							enter: 'animated fadeInDown',
-							exit: 'animated fadeOutUp'
-						},
-						onShow: null,
-						onShown: null,
-						onClose: null,
-						onClosed: null,
-						icon_type: 'class',
-					});
-
+					notificacao('Atenção!', val, 'top', 'right', 'danger', 'withicon', '#', '');
 					$("input[name='" + key + "']").addClass("has-error has-feedback");
-
 				})
 			},
-
-			error: function(xhr) {
-
-				$.notify({
-					// options
-					icon: 'flaticon-exclamation',
-					title: 'ERRO',
-					message: 'Não foi possível processar sua solicitação',
-					target: '_blank'
-				}, {
-					// settings
-					element: 'body',
-					position: null,
-					type: "danger",
-					allow_dismiss: true,
-					newest_on_top: false,
-					showProgressbar: false,
-					placement: {
-						from: "top",
-						align: "right"
-					},
-					offset: 20,
-					spacing: 10,
-					z_index: 1031,
-					delay: 5000,
-					timer: 1000,
-					url_target: '_blank',
-					mouse_over: null,
-					animate: {
-						enter: 'animated fadeInDown',
-						exit: 'animated fadeOutUp'
-					},
-					onShow: null,
-					onShown: null,
-					onClose: null,
-					onClosed: null,
-					icon_type: 'class',
-				});
-
-			}
 		},
-
 		complete: function() {
 
 		}
 	})
-
 })
-
-
-//$("#submit-diagnostico").click(function() {
-//	var diagnostico = {};
-//	diagnostico['atendimento'] = idAtendimento;
-//	diagnostico['prontuario'] = idProntuario;
-//	diagnostico.nota = tinymce.get("nota").getContent();
-//	diagnostico['cid'] = $("#id-cid").val();
-//
-//	$.ajax({
-//		url: '/diagnostico/',
-//		method: 'post',
-//		data: diagnostico,
-//		success: function() {
-//			$("#table-diagnosticos").DataTable().ajax.reload();
-//			limpaInputsDiagnostico();
-//			$.notify({
-//				// options
-//				icon: 'flaticon-success',
-//				title: 'SUCESSO',
-//				message: 'O diagnóstico foi salvo',
-//				target: '_blank'
-//			}, {
-//				// settings
-//				element: 'body',
-//				position: null,
-//				type: "success",
-//				allow_dismiss: true,
-//				newest_on_top: false,
-//				showProgressbar: false,
-//				placement: {
-//					from: "top",
-//					align: "right"
-//				},
-//				offset: 20,
-//				spacing: 10,
-//				z_index: 1031,
-//				delay: 5000,
-//				timer: 1000,
-//				url_target: '_blank',
-//				mouse_over: null,
-//				animate: {
-//					enter: 'animated fadeInDown',
-//					exit: 'animated fadeOutUp'
-//				},
-//				onShow: null,
-//				onShown: null,
-//				onClose: null,
-//				onClosed: null,
-//				icon_type: 'class',
-//			});
-//		},
-//
-//		error: function(xhr) {
-//
-//			$.notify({
-//				// options
-//				icon: 'flaticon-exclamation',
-//				title: 'ERRO',
-//				message: 'Não foi possível processar sua solicitação',
-//				target: '_blank'
-//			}, {
-//				// settings
-//				element: 'body',
-//				position: null,
-//				type: "danger",
-//				allow_dismiss: true,
-//				newest_on_top: false,
-//				showProgressbar: false,
-//				placement: {
-//					from: "top",
-//					align: "right"
-//				},
-//				offset: 20,
-//				spacing: 10,
-//				z_index: 1031,
-//				delay: 5000,
-//				timer: 1000,
-//				url_target: '_blank',
-//				mouse_over: null,
-//				animate: {
-//					enter: 'animated fadeInDown',
-//					exit: 'animated fadeOutUp'
-//				},
-//				onShow: null,
-//				onShown: null,
-//				onClose: null,
-//				onClosed: null,
-//				icon_type: 'class',
-//			});
-//
-//		}
-//	})
-//
-//})
-
-////Função pesquisa de Cids
-//$("#diagnostico-cid").autocomplete({
-//	source: "/cid/buscar",
-//	focus: function(event, ui) {
-//		$("#alergia-cid").val(ui.item.codigo + " - " + ui.item.nome);
-//		return false;
-//	},
-//	select: function(event, ui) {
-//		$("#diagnostico-cid").val(ui.item.codigo + " - " + ui.item.nome);
-//		$("#id-cid").val(ui.item.id);
-//		return false;
-//	}
-//}).autocomplete("instance")._renderItem = function(ul, item) {
-//	return $("<li>")
-//		.append("<div class='h6'>" + item.codigo + " - " + item.nome + "</div>").appendTo(ul);
-//}
-//Fim da função pesquisa Cids
-
-////Inicio da funcao atualizar Diagnósticos
-//function atualizaDiagnostico() {
-//	$("#table-diagnosticos").DataTable({
-//		responsive: true,
-//		paging: false,
-//		searching: false,
-//		ordering: false,
-//		ajax: {
-//			url: '/diagnostico/listar/atendimento/' + idAtendimento,
-//			dataSrc: ''
-//		},
-//		columns: [
-//			{
-//				title: 'NOTA',
-//				data: 'nota',
-//			},
-//			{
-//				title: 'CID',
-//				data: 'cid.codigo',
-//			},
-//			{
-//				title: 'DESCRICÃO',
-//				data: 'cid.nome',
-//			},
-//			{
-//				title: 'AÇÕES',
-//				data: 'id',
-//				mRender: function(data) {
-//					var retorno =
-//						" <button type='button' class='btn btn-warning btn-sm' data-value='" + data + "' onclick='excluirDiagnostico(this)'><i class='fa fa-trash'></i> Excluir </button>"
-//					return retorno;
-//				}
-//			}
-//		]
-//	})
-//};
-
-//function limpaInputsDiagnostico() {
-//	$("#nota").val("");
-//	$("#diagnostico-cid").val("");
-//	$("#id-cid").val("");
-//}
-
-//function excluirDiagnostico(element) {
-//	var idDiagnostico = $(element).attr("data-value");
-//	swal({
-//		title: 'Tem certeza que deseja excluir este CID?',
-//		text: "Você não poderá reverter esta ação!",
-//		icon: 'warning',
-//		buttons: {
-//			cancel: {
-//				visible: true,
-//				text: 'Não, cancelar!',
-//				className: 'btn btn-success btn-border'
-//			},
-//			confirm: {
-//				text: 'Sim, excluir!',
-//				className: 'btn btn-success'
-//			}
-//		}
-//	}).then((willDelete) => {
-//		if (willDelete) {
-//			$.ajax({
-//				url: '/diagnostico/' + idDiagnostico,
-//				method: 'delete',
-//				success: function() {
-//					$("#table-diagnosticos").DataTable().ajax.reload();
-//					swal("Sucesso! O Daiagnóstico foi excluido!", {
-//						icon: "success",
-//						buttons: {
-//							confirm: {
-//								className: 'btn btn-success'
-//							}
-//						}
-//					});
-//					atualizaCidAtestado();
-//				},
-//				statusCode: {
-//					403: function(xhr) {
-//						swal("Houve um erro!", xrh.reponseText, {
-//							icon: "error",
-//							buttons: {
-//								confirm: {
-//									className: 'btn btn-danger'
-//								}
-//							},
-//						});
-//					}
-//				}
-//			})
-//		} else {
-//			swal("Certo, não iremos excluir!", {
-//				buttons: {
-//					confirm: {
-//						className: 'btn btn-success'
-//					}
-//				}
-//			});
-//		}
-//	});
-//}
 
 function verificaConsulta() {
 	$.ajax({
@@ -490,7 +147,7 @@ function verificaConsulta() {
 			tinymce.get("historia-clinica").setMode('readonly');
 			tinymce.get("avaliacao").setMode('readonly');
 			tinymce.get("conduta").setMode('readonly');
-			
+
 		},
 
 		statusCode: {
@@ -508,6 +165,5 @@ function editarConsulta() {
 	tinymce.get("historia-clinica").setMode('design');
 	tinymce.get("avaliacao").setMode('design');
 	tinymce.get("conduta").setMode('design');
-
 	$("#card-action").empty().append("<button class='btn btn-primary'> Salvar</button>");
 }
