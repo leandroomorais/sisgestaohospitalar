fa20d5d77e50065d87e7c83fa596430650ae63a0
leandroomorais <leandromorais.dev@outlook.com>
2023-02-28 00:30:19 -0300

    adicionando migrations

---
+++ src/main/resources/db/migration/V2__popular_tabelas_role_e_tipo_usuario.sql
@@ -0,0 +1,9 @@
+INSERT INTO tipo_usuario (nome) VALUE ("MEDICO");
+INSERT INTO tipo_usuario (nome) VALUE ("ENFERMEIRO");
+INSERT INTO tipo_usuario (nome) VALUE ("AUXILIAR");
+INSERT INTO tipo_usuario (nome) VALUE ("ADMINISTRADOR");
+
+INSERT INTO role (nome) VALUE ("MEDICO");
+INSERT INTO role (nome) VALUE ("ENFERMEIRO");
+INSERT INTO role (nome) VALUE ("AUXILIAR");
+INSERT INTO role (nome) VALUE ("ADMINISTRADOR");
---
+++ src/main/resources/db/migration/V5__popular_estados.sql
@@ -0,0 +1,31 @@
+/*
+-- Query: SELECT * FROM sghtaboleiro.estado
+-- Date: 2023-02-27 23:27
+*/
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (1,'ACRE','AC',12);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (2,'ALAGOAS','AL',27);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (3,'AMAZONAS','AM',13);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (4,'AMAPÁ','AP',16);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (5,'BAHIA','BA',29);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (6,'CEARÁ','CE',23);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (7,'DISTRITO FEDERAL','DF',53);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (8,'ESPÍRITO SANTO','ES',32);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (9,'GOIÁS','GO',52);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (10,'MARANHÃO','MA',21);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (11,'MINAS GERAIS','MG',31);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (12,'MATO GROSSO DO SUL','MS',50);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (13,'MATO GROSSO','MT',51);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (14,'PARÁ','PA',15);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (15,'PARAÍBA','PB',25);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (16,'PERNAMBUCO','PE',26);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (17,'PIAUÍ','PI',22);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (18,'PARANÁ','PR',41);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (19,'RIO DE JANEIRO','RJ',33);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (20,'RIO GRANDE DO NORTE','RN',24);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (21,'RONDÔNIA','RO',11);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (22,'RORAIMA','RR',14);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (23,'RIO GRANDE DO SUL','RS',43);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (24,'SANTA CATARINA','SC',42);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (25,'SERGIPE','SE',28);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (26,'SÃO PAULO','SP',35);
+INSERT INTO estado (`id`,`nome`,`sigla`,`codigo`) VALUES (27,'TOCANTINS','TO',17);
