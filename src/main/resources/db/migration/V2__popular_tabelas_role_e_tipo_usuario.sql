d740ec594af7b29d79e296d7a1251601844799af
leandroomorais <leandromorais.dev@outlook.com>
2023-02-28 00:58:51 -0300

    ajustes

--- src/main/resources/db/migration/V2__popular_tabelas_role_e_tipo_usuario.sql
+++ src/main/resources/db/migration/V2__popular_tabelas_role_e_tipo_usuario.sql
@@ -2,8 +2,10 @@ INSERT INTO tipo_usuario (nome) VALUE ("MEDICO");
 INSERT INTO tipo_usuario (nome) VALUE ("ENFERMEIRO");
 INSERT INTO tipo_usuario (nome) VALUE ("AUXILIAR");
 INSERT INTO tipo_usuario (nome) VALUE ("ADMINISTRADOR");
+INSERT INTO tipo_usuario (nome) VALUE ("TECNICO");
 
 INSERT INTO role (nome) VALUE ("MEDICO");
 INSERT INTO role (nome) VALUE ("ENFERMEIRO");
 INSERT INTO role (nome) VALUE ("AUXILIAR");
 INSERT INTO role (nome) VALUE ("ADMINISTRADOR");
+INSERT INTO role (nome) VALUE ("TECNICO");
